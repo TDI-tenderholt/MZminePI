6e6eaf6092f291ccf9f028d5ec991de01f294afb
dan.schmidt <dan.schmidt@8f191f92-7a15-11e2-b7ab-c922b3a7170b>
2013-03-12 18:21:31 +0000

    Dump scans as text files (including checksums) Import centroided peaks lists from files if they exist
    git-svn-id: https://tdi.codebasehq.com/protid/mzminepi.svn@14 8f191f92-7a15-11e2-b7ab-c922b3a7170b

--- src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/massdetection/Veritomyx/Veritomyx.java
+++ src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/massdetection/Veritomyx/Veritomyx.java
@@ -19,204 +19,128 @@
 
 package net.sf.mzmine.modules.rawdatamethods.peakpicking.massdetection.Veritomyx;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.TreeSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 import javax.annotation.Nonnull;
 
 import net.sf.mzmine.data.DataPoint;
+import net.sf.mzmine.data.RawDataFile;
 import net.sf.mzmine.data.Scan;
 import net.sf.mzmine.modules.rawdatamethods.peakpicking.massdetection.MassDetector;
 import net.sf.mzmine.parameters.ParameterSet;
-import net.sf.mzmine.util.DataPointSorter;
-import net.sf.mzmine.util.SortingDirection;
-import net.sf.mzmine.util.SortingProperty;
+import FileChecksum.FileChecksum;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 
 public class Veritomyx implements MassDetector
 {
-    /**
-     * @see net.sf.mzmine.modules.peakpicking.threestep.massdetection.MassDetector#getMassValues(net.sf.mzmine.data.Scan)
-     */
-    public DataPoint[] getMassValues(Scan scan, ParameterSet parameters)
-    {
-		double noiseLevel = parameters.getParameter(VeritomyxParameters.noiseLevel).getValue();
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+	private boolean scans_dumped = false;
 
-		// Create a tree set of detected mzPeaks sorted by MZ in ascending order
-		TreeSet<ScanMzDataPoint> mzPeaks = new TreeSet<ScanMzDataPoint>(
-			new DataPointSorter(SortingProperty.MZ,
-				SortingDirection.Ascending));
-
-		// Create a tree set of candidate mzPeaks sorted by intensity in
-		// descending order.
-		TreeSet<ScanMzDataPoint> candidatePeaks = new TreeSet<ScanMzDataPoint>(
-			new DataPointSorter(SortingProperty.Intensity,
-				SortingDirection.Descending));
-
-		// First get all candidate peaks (local maximum)
-		getLocalMaxima(scan, candidatePeaks, noiseLevel);
-
-		// We calculate the exact mass for each peak,
-		// starting with biggest intensity peak and so on
-		while (candidatePeaks.size() > 0)
-		{
-		    // Always take the biggest (intensity) peak
-		    ScanMzDataPoint currentCandidate = candidatePeaks.first();
-	
-		    // Calculate the exact mass and update value in current candidate (MzPeak)
-		    double exactMz = calculateExactMass(currentCandidate);
-		    currentCandidate.setMZ(exactMz);
-	
-		    // Add this candidate to the final tree set sorted by MZ and remove from tree set sorted by intensity
-		    mzPeaks.add(currentCandidate);
-		    candidatePeaks.remove(currentCandidate);
-		}
-
-		// Return an array of detected MzPeaks sorted by MZ
-		return mzPeaks.toArray(new ScanMzDataPoint[0]);
-    }
-
-    /**
-     * This method gets all possible MzPeaks using local maximum criteria from
-     * the current scan and return a tree set of MzPeaks sorted by intensity in
-     * descending order.
-     * 
-     * @param scan
-     * @return
-     */
-    private void getLocalMaxima(Scan scan, TreeSet<ScanMzDataPoint> candidatePeaks, double noiseLevel)
-    {
-		DataPoint[] scanDataPoints = scan.getDataPoints();
-		if (scanDataPoints.length == 0)
-		    return;
-		DataPoint localMaximum = scanDataPoints[0];
-		ArrayList<DataPoint> rangeDataPoints = new ArrayList<DataPoint>();
-
-		boolean ascending = true;
-
-		// Iterate through all data points
-		for (int i = 0; i < scanDataPoints.length - 1; i++)
-		{
-		    boolean nextIsBigger = scanDataPoints[i + 1].getIntensity() > scanDataPoints[i].getIntensity();
-		    boolean nextIsZero = scanDataPoints[i + 1].getIntensity() == 0;
-		    boolean currentIsZero = scanDataPoints[i].getIntensity() == 0;
-
-		    // Ignore zero intensity regions
-		    if (currentIsZero)
-		    	continue;
-
-		    // Add current (non-zero) data point to the current m/z peak
-		    rangeDataPoints.add(scanDataPoints[i]);
-
-		    // Check for local maximum
-		    if (ascending && (!nextIsBigger))
-		    {
-				localMaximum = scanDataPoints[i];
-				ascending = false;
-				continue;
-		    }
-
-		    // Check for the end of the peak
-		    if ((!ascending) && (nextIsBigger || nextIsZero))
-		    {
-				// Add the m/z peak if it is above the noise level
-				if (localMaximum.getIntensity() > noiseLevel)
-				{
-				    DataPoint[] rawDataPoints = rangeDataPoints.toArray(new DataPoint[0]);
-				    candidatePeaks.add(new ScanMzDataPoint(localMaximum, rawDataPoints));
-				}
-
-				// Reset and start with new peak
-				ascending = true;
-				rangeDataPoints.clear();
-		    }
-		}
-    }
-
-    /**
-     * This method calculates the exact mass of a peak using the FWHM concept
-     * and linear equation (y = mx + b).
-     * 
-     * @param ExactMassDataPoint
-     * @return double
-     */
-    private double calculateExactMass(ScanMzDataPoint currentCandidate)
+	/**
+	 * 
+	 */
+	public DataPoint[] getMassValues(Scan scan, ParameterSet parameters)
 	{
-		/*
-		 * According with the FWHM concept, the exact mass of this peak is the
-		 * half point of FWHM. In order to get the points in the curve that
-		 * define the FWHM, we use the linear equation.
-		 * 
-		 * First we look for, in left side of the peak, 2 data points together
-		 * that have an intensity less (first data point) and bigger (second
-		 * data point) than half of total intensity. Then we calculate the slope
-		 * of the line defined by this two data points. At least, we calculate
-		 * the point in this line that has an intensity equal to the half of
-		 * total intensity
-		 * 
-		 * We repeat the same process in the right side.
-		 */
+		int     first_scan = parameters.getParameter(VeritomyxParameters.first_scan).getValue();
+		int     last_scan  = parameters.getParameter(VeritomyxParameters.last_scan).getValue();
+		boolean dump_scans = parameters.getParameter(VeritomyxParameters.dump_scans).getValue();
+		boolean read_peaks = parameters.getParameter(VeritomyxParameters.read_peaks).getValue();
+		
+		String datafilename = scan.getDataFile().getName();
 
-		double xRight = -1, xLeft = -1;
-		double halfIntensity = currentCandidate.getIntensity() / 2;
-		DataPoint[] rangeDataPoints = currentCandidate.getRawDataPoints();
+		RawDataFile rawdata = scan.getDataFile();
+		ArrayList<VeritomyxMzDataPoint> mzPeaks = new ArrayList<VeritomyxMzDataPoint>();
 
-		for (int i = 0; i < rangeDataPoints.length - 1; i++)
+		if (dump_scans && !scans_dumped)
 		{
-		    // Left side of the curve
-		    if ((rangeDataPoints[i].getIntensity() <= halfIntensity)
-			    && (rangeDataPoints[i].getMZ() < currentCandidate.getMZ())
-			    && (rangeDataPoints[i + 1].getIntensity() >= halfIntensity))
-		    {
-				// First point with intensity just less than half of total intensity
-				double leftY1 = rangeDataPoints[i].getIntensity();
-				double leftX1 = rangeDataPoints[i].getMZ();
+			for (int s = first_scan; s < last_scan; s++)
+			{
+				scan = rawdata.getScan(s);
+				if (scan == null)
+					continue;
 
-				// Second point with intensity just bigger than half of total intensity
-				double leftY2 = rangeDataPoints[i + 1].getIntensity();
-				double leftX2 = rangeDataPoints[i + 1].getMZ();
-
-				// We calculate the slope with formula m = Y1 - Y2 / X1 - X2
-				double mLeft = (leftY1 - leftY2) / (leftX1 - leftX2);
-
-				// We calculate the desired point (at half intensity) with the linear equation
-				// X = X1 + [(Y - Y1) / m ], where Y = half of total intensity
-				xLeft = leftX1 + (((halfIntensity) - leftY1) / mLeft);
-				continue;
-		    }
-
-		    // Right side of the curve
-		    if ((rangeDataPoints[i].getIntensity() >= halfIntensity)
-			    && (rangeDataPoints[i].getMZ() > currentCandidate.getMZ())
-			    && (rangeDataPoints[i + 1].getIntensity() <= halfIntensity))
-		    {
-				// First point with intensity just bigger than half of total intensity
-				double rightY1 = rangeDataPoints[i].getIntensity();
-				double rightX1 = rangeDataPoints[i].getMZ();
-
-				// Second point with intensity just less than half of total intensity
-				double rightY2 = rangeDataPoints[i + 1].getIntensity();
-				double rightX2 = rangeDataPoints[i + 1].getMZ();
-
-				// We calculate the slope with formula m = Y1 - Y2 / X1 - X2
-				double mRight = (rightY1 - rightY2) / (rightX1 - rightX2);
-
-				// We calculate the desired point (at half intensity) with the linear equation
-				// X = X1 + [(Y - Y1) / m ], where Y = half of total intensity
-				xRight = rightX1 + (((halfIntensity) - rightY1) / mRight);
-				break;
-		    }
+				String line;
+				String scanfilename = datafilename + ".scan_" + s + ".txt";
+				logger.info("Saving scan to " + scanfilename);
+				DataPoint points[] = scan.getDataPoints();
+				try
+				{
+					File scanfile = new File(scanfilename);
+					PrintWriter fd = new PrintWriter(scanfile);
+					FileChecksum fchksum = new FileChecksum(scanfile);
+					fd.println("# Raw File: " + scan.getDataFile().getName());
+					fd.println("# Scan: "     + scan.getScanNumber());
+					fd.println("# MS Level: " + scan.getMSLevel());
+					for (int i = 0; i < scan.getNumberOfDataPoints(); i++)
+					{
+						line = points[i].getMZ() + "\t" + points[i].getIntensity();
+						fd.println(line);
+						fchksum.hash_line(line);
+					}
+					fd.close();
+					fchksum.append_txt(false);
+					fd = null;
+				}
+				catch (Exception e)
+				{
+					logger.info(e.getMessage());
+					e.printStackTrace();
+					return mzPeaks.toArray(new DataPoint[0]);
+				}
+			}
+			scans_dumped = true;
 		}
 
-		// We verify the values to confirm we find the desired points. If not we
-		// return the same mass value.
-		if ((xRight == -1) || (xLeft == -1))
-		    return currentCandidate.getMZ();
+		if (read_peaks)
+		{
+			int s = scan.getScanNumber();
+			if ((s < first_scan) || (s < last_scan))
+			{
+				return mzPeaks.toArray(new DataPoint[0]);
+			}
 
-		// The center of left and right points is the exact mass of our peak.
-		double exactMass = (xLeft + xRight) / 2;
+			String centfilename = scan.getDataFile().getName() + ".scan_" + s + "_cent.txt";
+			logger.info("Reading centroided data from " + centfilename);
+			try
+			{
+				File centfile = new File(centfilename);
+				FileChecksum fchksum = new FileChecksum(centfile);
+				if (fchksum.verify(false))
+				{
+					throw new IOException("Invalid checksum in centroided file " + centfilename);
+				}
+				List<String> lines = Files.readLines(centfile , Charsets.UTF_8);
+				for (String line:lines)
+				{
+					if (line.startsWith("#"))	// skip comment lines
+						continue;
 
-		return exactMass;
-	}
+					Scanner sc = new Scanner(line);
+					double mz  = sc.nextDouble();
+					double y   = sc.nextDouble();
+					mzPeaks.add(new VeritomyxMzDataPoint(mz, y));
+					sc.close();
+				}
+			}
+			catch (Exception e)
+			{
+				logger.info(e.getMessage());
+				e.printStackTrace();
+				return mzPeaks.toArray(new DataPoint[0]);
+			}
+		}
+
+		// Return an array of detected peaks sorted by MZ
+		return mzPeaks.toArray(new DataPoint[0]);
+    }
 
     public @Nonnull String getName()
     {
