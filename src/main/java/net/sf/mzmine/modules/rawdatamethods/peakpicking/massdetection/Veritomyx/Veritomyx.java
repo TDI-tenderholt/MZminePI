27e8d1ab132edbe4a1519ab284dbc6ba758df6e6
dan.schmidt <dan.schmidt@8f191f92-7a15-11e2-b7ab-c922b3a7170b>
2013-03-30 00:06:53 +0000

    First cut at calling a URL.  Had to be http not https to avoid untrusted cert.
    git-svn-id: https://tdi.codebasehq.com/protid/mzminepi.svn@23 8f191f92-7a15-11e2-b7ab-c922b3a7170b

--- src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/massdetection/Veritomyx/Veritomyx.java
+++ src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/massdetection/Veritomyx/Veritomyx.java
@@ -19,8 +19,16 @@
 
 package net.sf.mzmine.modules.rawdatamethods.peakpicking.massdetection.Veritomyx;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -67,6 +75,36 @@ public class Veritomyx implements MassDetector
 				scan.exportToFile("", "");
 			}
 			scans_dumped = true;
+		}
+		
+		try {
+			String args = "p=" + URLEncoder.encode("1000", "UTF-8");
+			args += "&Version=1.2.4";
+			URL url = new URL("http://test.veritomyx.com/vtmx/interface/vtmx_batch_internal.php");
+			URLConnection connection = url.openConnection();
+
+			connection.setDoOutput(true);
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+			out.write(args);
+			out.close();
+
+	        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String decodedString;
+			while ((decodedString = in.readLine()) != null)
+			{
+				System.out.println(decodedString);
+			}
+			in.close();
+
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		if (read_peaks)
