ecd427161f92579e682d6c5aa2ad0b32e596c53a
dan.schmidt <dan.schmidt@8f191f92-7a15-11e2-b7ab-c922b3a7170b>
2013-06-29 17:31:40 +0000

    Can read and write compressed files. Have put and get working over SFTP.
    git-svn-id: https://tdi.codebasehq.com/protid/mzminepi.svn@43 8f191f92-7a15-11e2-b7ab-c922b3a7170b

--- src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/massdetection/Veritomyx/Veritomyx.java
+++ src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/massdetection/Veritomyx/Veritomyx.java
@@ -38,7 +38,6 @@ import java.util.logging.Logger;
 import javax.annotation.Nonnull;
 
 import net.sf.mzmine.data.DataPoint;
-import net.sf.mzmine.data.RawDataFile;
 import net.sf.mzmine.data.Scan;
 import net.sf.mzmine.data.impl.SimpleDataPoint;
 import net.sf.mzmine.modules.rawdatamethods.peakpicking.massdetection.MassDetector;
@@ -50,217 +49,285 @@ import net.sf.opensftp.SftpUtil;
 import net.sf.opensftp.SftpUtilFactory;
 import FileChecksum.FileChecksum;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
-
 public class Veritomyx implements MassDetector
 {
-	private Logger logger = Logger.getLogger(this.getClass().getName());
-	private boolean scans_dumped = false;
+	private Logger logger       = Logger.getLogger(this.getClass().getName());
+	private int last_scan_num   = -1;
+	private SftpUtil sftp       = SftpUtilFactory.getSftpUtil();
+	private SftpSession session = null;
+	private String host         = "boman";
+	private String user         = "dschmidt";
+	private String password     = "vtmx.eldersdi";
+	private String dir          = "batches";
 
-	private void transferFile(String fname)
+	/**
+	 * Open the SFTP session
+	 */
+	private boolean _open_sftp_session()
 	{
-		SftpUtil util = SftpUtilFactory.getSftpUtil();
-		SftpSession session = null;
-		SftpResult  result;
-		String host     = "boman";
-		String user     = "dschmidt";
-		String password = "vtmx.eldersdi";
-		String dir      = "batches";
-
-		logger.info("Copy " + fname + " to " + user + "@" + host + ":" + dir);
-
-		try {
-			session = util.connectByPasswdAuth(host, 22, user, password, SftpUtil.STRICT_HOST_KEY_CHECKING_OPTION_NO, 10000);
-		} catch (SftpException e) {
-			logger.info("Error: Cannot connect to SFTP server " + user + "@" + host);
-			e.printStackTrace();
-			return;
-		}
-
-		result = util.cd(session, dir);
-		if (!result.getSuccessFlag())
+		if (session == null)
 		{
-			result = util.mkdir(session, dir);
+			try {
+				session = sftp.connectByPasswdAuth(host, 22, user, password, SftpUtil.STRICT_HOST_KEY_CHECKING_OPTION_NO, 10000);
+			} catch (SftpException e) {
+				session = null;
+				logger.info("Error: Cannot connect to SFTP server " + user + "@" + host);
+				e.printStackTrace();
+				return false;
+			}
+			SftpResult result = sftp.cd(session, dir);
 			if (!result.getSuccessFlag())
 			{
-				logger.info("Error: Cannot find or create " + dir + " directory");
-				return;
+				result = sftp.mkdir(session, dir);
+				if (!result.getSuccessFlag())
+				{
+					logger.info("Error: Cannot create remote directory, " + dir);
+					return false;
+				}
+				result = sftp.cd(session, dir);
+				if (!result.getSuccessFlag())
+				{
+					logger.info("Error: Cannot find remote directory, " + dir);
+					return false;
+				}
 			}
 		}
-		result = util.rm(session, fname);
-		result = util.put(session, fname, fname + ".filepart");
-		if (!result.getSuccessFlag())
-		{
-			logger.info("Error: Cannot write file: " + fname);
-			return;
-		}
-		result = util.rename(session, fname + ".filepart", fname); //rename a remote file
-		if (!result.getSuccessFlag())
-		{
-			logger.info("Error: Cannot rename file: " + fname);
-			return;
-		}
-		util.disconnect(session); //quit
+		return true;
 	}
 
 	/**
+	 * Close the SFTP session
+	 */
+	private void _close_sftp_session()
+	{
+		sftp.disconnect(session);
+		session = null;
+	}
+
+	/**
+	 * Transfer the given file to SFTP drop
 	 * 
+	 * @param fname
+	 */
+	private boolean _sftp_put_file(String fname)
+	{
+		SftpResult result;
+
+		logger.info("Put " + user + "@" + host + ":" + dir + "/" + fname);
+		if (!_open_sftp_session())
+			return false;
+
+		result = sftp.rm(session, fname);
+		result = sftp.put(session, fname, fname + ".filepart");
+		if (!result.getSuccessFlag())
+		{
+			logger.info("Error: Cannot write file: " + fname);
+			return false;
+		}
+		result = sftp.rename(session, fname + ".filepart", fname); //rename a remote file
+		if (!result.getSuccessFlag())
+		{
+			logger.info("Error: Cannot rename file: " + fname);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Transfer the given file from SFTP drop
+	 * 
+	 * @param fname
+	 */
+	private boolean _sftp_get_file(String fname)
+	{
+		SftpResult  result;
+
+		logger.info("Get " + user + "@" + host + ":" + dir + "/" + fname);
+		if (!_open_sftp_session())
+			return false;
+
+		result = sftp.get(session, fname);
+		if (!result.getSuccessFlag())
+		{
+			logger.info("Error: Cannot read file: " + fname);
+			return false;
+		}
+
+		return true;
+	}
+
+	private void _web_page()
+	{
+		String exsistingFileName = "";
+		try {
+			// build the URL parameters
+			String args = "?Version=" + "1.2.6";
+			args += "&p=" + URLEncoder.encode("1000", "UTF-8");
+			args += "&User=" + URLEncoder.encode("regression@veritomyx.com", "UTF-8");
+			args += "&Code=" + URLEncoder.encode("joe3test", "UTF-8");
+			args += "&Action=" + "upload";
+			args += "&Size=" + "0";
+			args += "&Fcnt=" + "0";
+			args += "&Files=" + "0";
+			args += "&Pcmd=" + "ctr";	// Centroid
+			args += "&Tarf=" + URLEncoder.encode(exsistingFileName, "UTF-8");
+			args += "&Over=" + "0";
+			System.out.println(args + "\n");
+
+			String boundary = "***232404jkg4220957934FW**";
+
+			URL url = new URL("http://test.veritomyx.com/vtmx/interface/vtmx_batch_internal_t.php" + args);
+			HttpURLConnection uc = (HttpURLConnection)url.openConnection();
+
+			uc.setDoOutput(true);
+			uc.setDoInput(true);
+			uc.setAllowUserInteraction(false);
+			uc.setUseCaches(false);
+			uc.setRequestMethod("POST");
+			uc.setRequestProperty("Connection", "Keep-Alive");
+			uc.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+			DataOutputStream dos = new DataOutputStream(uc.getOutputStream());
+			dos.writeBytes("--" + boundary + "\r\n");
+			dos.writeBytes("Content-Disposition: form-data; name=\"userfile\";" + " filename=\"" + exsistingFileName + "\"" + "\r\n");
+			dos.writeBytes("\r\n");
+
+			// create a buffer of maximum size
+			FileInputStream fileInputStream = new FileInputStream(new File(exsistingFileName));
+			int maxBufferSize = 1 * 1024 * 1024;;
+			int bytesAvailable = fileInputStream.available();
+			int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+			byte[] buffer = new byte[bufferSize];
+
+			// read file and write it into form...
+			int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+			while (bytesRead > 0)
+			{
+				dos.write(buffer, 0, bytesRead);
+				bytesAvailable = fileInputStream.available();
+				bufferSize = Math.min(bytesAvailable, maxBufferSize);
+				bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+			}
+
+			// send multi-part form data necessary after file data...
+			dos.writeBytes("\r\n--" + boundary + "--\r\n");
+
+			// close streams
+			fileInputStream.close();
+			dos.flush();
+			dos.close();
+
+			// Read the response from the HTTP server
+			BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+			String decodedString;
+			while ((decodedString = in.readLine()) != null)
+			{
+				System.out.println(decodedString);
+			}
+			in.close();
+
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Compute the peaks list for the given scan
+	 * 
+	 * @param scan
+	 * @param parameters
+	 * @return
 	 */
 	public DataPoint[] getMassValues(Scan scan, ParameterSet parameters)
 	{
-		int     first_scan = parameters.getParameter(VeritomyxParameters.first_scan).getValue();
-		int     last_scan  = parameters.getParameter(VeritomyxParameters.last_scan).getValue();
 		boolean dump_scans = parameters.getParameter(VeritomyxParameters.dump_scans).getValue();
 		boolean read_peaks = parameters.getParameter(VeritomyxParameters.read_peaks).getValue();
-		
-		RawDataFile rawdata = scan.getDataFile();
-		ArrayList<DataPoint> mzPeaks = new ArrayList<DataPoint>();
+		int     first_scan = parameters.getParameter(VeritomyxParameters.first_scan).getValue();
+		int     last_scan  = parameters.getParameter(VeritomyxParameters.last_scan).getValue();
 
-		if (dump_scans && !scans_dumped)
+		List<DataPoint> mzPeaks = null;
+		int scan_num = scan.getScanNumber();
+		if ((scan_num >= first_scan) && (scan_num <= last_scan))	// only process scans within requested range
 		{
-			for (int s = first_scan; s <= last_scan; s++)
+			String filename = scan.exportFilename("") + ".gz";
+	
+			if (dump_scans)
 			{
-				Scan tmpscan = rawdata.getScan(s);
-				if (tmpscan != null)
-				{
-					String fileName = scan.exportFilename("");
-					tmpscan.exportToFile("", fileName, true);
-					transferFile(fileName);
-				}
+				scan.exportToFile("", filename);
+				_sftp_put_file(filename);
 			}
-			scans_dumped = true;
-
-			try {
-				// build the tar file
-				String exsistingFileName = scan.exportFilename("");
-
-				// build the URL parameters
-				String args = "?Version=" + "1.2.6";
-				args += "&p=" + URLEncoder.encode("1000", "UTF-8");
-				args += "&User=" + URLEncoder.encode("regression@veritomyx.com", "UTF-8");
-				args += "&Code=" + URLEncoder.encode("joe3test", "UTF-8");
-				args += "&Action=" + "upload";
-				args += "&Size=" + "0";
-				args += "&Fcnt=" + "0";
-				args += "&Files=" + "0";
-				args += "&Pcmd=" + "ctr";	// Centroid
-				args += "&Tarf=" + URLEncoder.encode(exsistingFileName, "UTF-8");
-				args += "&Over=" + "0";
-				System.out.println(args + "\n");
-
-				String boundary = "***232404jkg4220957934FW**";
-
-				URL url = new URL("http://test.veritomyx.com/vtmx/interface/vtmx_batch_internal_t.php" + args);
-				HttpURLConnection uc = (HttpURLConnection)url.openConnection();
-
-				uc.setDoOutput(true);
-				uc.setDoInput(true);
-				uc.setAllowUserInteraction(false);
-				uc.setUseCaches(false);
-				uc.setRequestMethod("POST");
-				uc.setRequestProperty("Connection", "Keep-Alive");
-				uc.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-
-				DataOutputStream dos = new DataOutputStream(uc.getOutputStream());
-				dos.writeBytes("--" + boundary + "\r\n");
-				dos.writeBytes("Content-Disposition: form-data; name=\"userfile\";" + " filename=\"" + exsistingFileName + "\"" + "\r\n");
-				dos.writeBytes("\r\n");
-
-				// create a buffer of maximum size
-				FileInputStream fileInputStream = new FileInputStream(new File(exsistingFileName));
-				int maxBufferSize = 1 * 1024 * 1024;;
-				int bytesAvailable = fileInputStream.available();
-				int bufferSize = Math.min(bytesAvailable, maxBufferSize);
-				byte[] buffer = new byte[bufferSize];
-
-				// read file and write it into form...
-				int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-				while (bytesRead > 0)
+	
+			if (read_peaks)
+			{
+				// convert filename to expected peak file name
+				String pfilename = filename.replace(".txt", ".vcent.txt");
+				logger.info("Reading centroided data from " + pfilename);
+				_sftp_get_file(pfilename);
+				try
 				{
-					dos.write(buffer, 0, bytesRead);
-					bytesAvailable = fileInputStream.available();
-					bufferSize = Math.min(bytesAvailable, maxBufferSize);
-					bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+					File centfile = new File(pfilename);
+					FileChecksum fchksum = new FileChecksum(centfile);
+					if (!fchksum.verify(false))
+						throw new IOException("Invalid checksum in centroided file " + pfilename);
+	
+					List<String> lines = fchksum.getFileStrings();
+					mzPeaks = new ArrayList<DataPoint>();
+					for (String line:lines)
+					{
+						if (line.startsWith("#") || line.isEmpty())	// skip comment lines
+							continue;
+	
+						Scanner sc = new Scanner(line);
+						double mz  = sc.nextDouble();
+						double y   = sc.nextDouble();
+						mzPeaks.add(new SimpleDataPoint(mz, y));
+						sc.close();
+					}
 				}
-
-				// send multi-part form data necessary after file data...
-				dos.writeBytes("\r\n--" + boundary + "--\r\n");
-
-				// close streams
-				fileInputStream.close();
-				dos.flush();
-				dos.close();
-
-				// Read the response from the HTTP server
-		        BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
-				String decodedString;
-				while ((decodedString = in.readLine()) != null)
+				catch (Exception e)
 				{
-					System.out.println(decodedString);
+					logger.info(e.getMessage());
+					e.printStackTrace();
 				}
-				in.close();
-
-			} catch (MalformedURLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 		}
 
-		if (read_peaks)
+		if (last_scan_num == -1)
 		{
-			int s = scan.getScanNumber();
-			if ((s < first_scan) || (s > last_scan))
-				return null;
-
-			String centfilename = scan.exportFilename("").replace(".txt", ".vcent.txt");
-			logger.info("Reading centroided data from " + centfilename);
-			try
-			{
-				File centfile = new File(centfilename);
-				FileChecksum fchksum = new FileChecksum(centfile);
-				if (!fchksum.verify(false))
-					throw new IOException("Invalid checksum in centroided file " + centfilename);
-
-				List<String> lines = Files.readLines(centfile , Charsets.UTF_8);
-				for (String line:lines)
-				{
-					if (line.startsWith("#") || line.isEmpty())	// skip comment lines
-						continue;
-
-					Scanner sc = new Scanner(line);
-					double mz  = sc.nextDouble();
-					double y   = sc.nextDouble();
-					mzPeaks.add(new SimpleDataPoint(mz, y));
-					sc.close();
-				}
-			}
-			catch (Exception e)
-			{
-				logger.info(e.getMessage());
-				e.printStackTrace();
-				return null;
-			}
+			int[] i = scan.getDataFile().getScanNumbers(scan.getMSLevel());
+			last_scan_num = i[i.length - 1];
+		}
+		if (scan_num == last_scan_num)	// last scan in set
+		{
+			_close_sftp_session();
 		}
 
-		// Return an array of detected peaks sorted by MZ
-		return mzPeaks.toArray(new DataPoint[0]);
-    }
+		if (mzPeaks != null)
+			return mzPeaks.toArray(new DataPoint[0]);	// Return an array of detected peaks sorted by MZ
+		return null;
+	}
 
-    public @Nonnull String getName()
-    {
-    	return "PeakInvestigator™";
-    }
+	/**
+	 * Return the name of this module
+	 * 
+	 * @return
+	 */
+	public @Nonnull String getName()
+	{
+		return "PeakInvestigator™";
+	}
 
-    @Override
-    public @Nonnull Class<? extends ParameterSet> getParameterSetClass()
-    {
-    	return VeritomyxParameters.class;
-    }
+	@Override
+	public @Nonnull Class<? extends ParameterSet> getParameterSetClass()
+	{
+		return VeritomyxParameters.class;
+	}
 }
