0eda90c9ae64c1101a317bee96635b6a63f1a4ef
dan.schmidt <dan.schmidt@8f191f92-7a15-11e2-b7ab-c922b3a7170b>
2013-12-13 08:23:30 +0000

    Set the current directory in the file chooser
    git-svn-id: https://tdi.codebasehq.com/protid/mzminepi.svn@154 8f191f92-7a15-11e2-b7ab-c922b3a7170b

--- src/main/java/net/sf/mzmine/modules/projectmethods/projectsave/ProjectSaveParameters.java
+++ src/main/java/net/sf/mzmine/modules/projectmethods/projectsave/ProjectSaveParameters.java
@@ -34,11 +34,9 @@ import net.sf.mzmine.util.ExitCode;
 
 public class ProjectSaveParameters extends SimpleParameterSet {
 
-	private static final FileFilter filters[] = new FileFilter[] { new FileNameExtensionFilter(
-			"MZmine projects", "mzmine") };
+	private static final FileFilter filters[] = new FileFilter[] { new FileNameExtensionFilter("MZmine projects", "mzmine") };
 
-	public static final FileNameParameter projectFile = new FileNameParameter(
-			"Project file", "File name of project to be saved");
+	public static final FileNameParameter projectFile = new FileNameParameter("Project file", "File name of project to be saved");
 
 	public ProjectSaveParameters() {
 		super(new Parameter[] { projectFile });
@@ -58,10 +56,16 @@ public class ProjectSaveParameters extends SimpleParameterSet {
 		for (FileFilter filter : filters)
 			chooser.setFileFilter(filter);
 
+		File currentFile = getParameter(projectFile).getValue();
+		if (currentFile != null) {
+			File currentDir = currentFile.getParentFile();
+			if ((currentDir != null) && (currentDir.exists()))
+				chooser.setCurrentDirectory(currentDir);
+		}
+
 		chooser.setMultiSelectionEnabled(false);
 
-		int returnVal = chooser.showSaveDialog(MZmineCore.getDesktop()
-				.getMainFrame());
+		int returnVal = chooser.showSaveDialog(MZmineCore.getDesktop().getMainFrame());
 		if (returnVal != JFileChooser.APPROVE_OPTION)
 			return ExitCode.CANCEL;
 
@@ -72,9 +76,8 @@ public class ProjectSaveParameters extends SimpleParameterSet {
 		}
 
 		if (selectedFile.exists()) {
-			int selectedValue = JOptionPane.showConfirmDialog(MZmineCore
-					.getDesktop().getMainFrame(), selectedFile.getName()
-					+ " already exists, overwrite ?", "Question...",
+			int selectedValue = JOptionPane.showConfirmDialog(MZmineCore.getDesktop().getMainFrame(),
+					selectedFile.getName() + " already exists, overwrite ?", "Question...",
 					JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
 			if (selectedValue != JOptionPane.YES_OPTION)
 				return ExitCode.CANCEL;
