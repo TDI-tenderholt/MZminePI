e80b85415c0f08ba219e0fd577286f48379a2aad
dan.schmidt <dan.schmidt@8f191f92-7a15-11e2-b7ab-c922b3a7170b>
2013-12-20 21:40:59 +0000

    Require new Web CLI interface on switch to AWS
    git-svn-id: https://tdi.codebasehq.com/protid/mzminepi.svn@183 8f191f92-7a15-11e2-b7ab-c922b3a7170b

--- src/main/java/net/sf/mzmine/main/MZmineCore.java
+++ src/main/java/net/sf/mzmine/main/MZmineCore.java
@@ -61,7 +61,7 @@ public final class MZmineCore
 	public static final String MZmineShortName = "MZminePI";
 
 	public static final boolean VtmxTest       = false;		// live or test server
-	public static final String  ReqVtmxVersion = "1.16"    + (VtmxTest ? "t" : "");	// Required Veritomyx CLI version (see VTMXweb:interface/version.inc.php)
+	public static final String  ReqVtmxVersion = "1.17"    + (VtmxTest ? "t" : "");	// Required Veritomyx CLI version (see VTMXweb:interface/version.inc.php)
 	public static final String  MZmineVersion  = "2.10.25" + (VtmxTest ? "t" : "");
 
 	private static Logger logger = Logger.getLogger(MZmineCore.class.getName());
