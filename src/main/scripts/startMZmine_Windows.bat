626cf11fd0f6790c28ec58712d94dbc690fe0c8c
dan.schmidt <dan.schmidt@8f191f92-7a15-11e2-b7ab-c922b3a7170b>
2014-03-28 22:19:03 +0000

    Adam's nicely edited version of windows start script
    git-svn-id: https://tdi.codebasehq.com/protid/mzminepi.svn@241 8f191f92-7a15-11e2-b7ab-c922b3a7170b

--- src/main/scripts/startMZmine_Windows.bat
+++ src/main/scripts/startMZmine_Windows.bat
@@ -1,52 +1,89 @@
 @echo off
 
-rem The HEAP_SIZE variable line defines the Java heap size in MB. 
-rem That is the total amount of memory available to MZmine 2.
-rem Please adjust according to the amount of memory of your computer.
-rem Maximum value on a 32-bit Windows system is about 1300. 
-set HEAP_SIZE=1024
+rem ***********************************************************************
+rem * 1. Set R environment variables. Choose appropriate version numbers.
+rem ***********************************************************************
+    set R_HOME=C:\Program Files\R\R-3.0.3
+    set R_LIBS_USER=%USERPROFILE%\Documents\R\win-library\3.0
 
-rem The TMP_FILE_DIRECTORY parameter defines the location where temporary 
-rem files (parsed raw data) will be placed. Default is %TEMP%, which 
-rem represents the system temporary directory.
-set TMP_FILE_DIRECTORY=%TEMP%
+rem ***********************************************************************
 
-rem Set R environment variables. Choose Version 3.0, 2.15 or your version.
-rem set R_HOME=C:\Program Files\R\R-2.15.3
-rem set R_LIBS_USER=%USERPROFILE%\Documents\R\win-library\2.15
-set R_HOME=C:\Program Files\R\R-3.0.3
-set R_LIBS_USER=%USERPROFILE%\Documents\R\win-library\3.0
 
-rem Set remaining R environment variables.
-set R_SHARE_DIR=%R_HOME%\share 
-set R_INCLUDE_DIR=%R_HOME%\include
-set R_DOC_DIR=%R_HOME%\doc
+rem ***********************************************************************
+rem * 2. Set remaining R environment variables.
+rem ***********************************************************************
+    set R_LIBS=%R_HOME%\library
+    set R_SHARE_DIR=%R_HOME%\share 
+    set R_INCLUDE_DIR=%R_HOME%\include
+    set R_DOC_DIR=%R_HOME%\doc
 
-rem Include R DLLs in PATH.
-set PATH=%PATH%;%R_HOME%\bin\x64
+rem ***********************************************************************
 
-rem The directory holding the JRI shared library (libjri.so).
-set JRI_LIB_PATH=%R_LIBS_USER%\rJava\jri\x64
 
-rem It is usually not necessary to modify the JAVA_COMMAND parameter, but 
-rem if you like to run a specific Java Virtual Machine, you may set the 
-rem path to the java command of that JVM
-set JAVA_COMMAND=java
+rem ***********************************************************************
+rem * 3. Include R DLLs in PATH, and set directory holding the shared 
+rem *    library (libjri.dll).
+rem ***********************************************************************
+    set PATH=%PATH%;%R_HOME%\bin\x64
+    set JRI_LIB_PATH=%R_LIBS_USER%\rJava\jri\x64;%R_LIBS%\rJava\jri\x64
 
-rem It is not necessary to modify the following section
+rem ***********************************************************************
+
+
+rem ***********************************************************************
+rem * 4. It is usually not necessary to modify the JAVA_COMMAND parameter, 
+rem *    but if you like to run a specific Java Virtual Machine, you may 
+rem *    set the path to the java command of that JVM.
+rem ***********************************************************************
+    set JAVA_COMMAND=java
+
+rem ***********************************************************************
+
+
+rem ***********************************************************************
+rem * 5. The HEAP_SIZE variable line defines the Java heap size in MB. 
+rem *    That is the total amount of memory available to MZmine 2.
+rem *    Please adjust according to the amount of memory of your computer.
+rem *    Maximum value on a 32-bit Windows system is about 1300.
+rem ***********************************************************************
+    set HEAP_SIZE=1024
+
+rem ***********************************************************************
+
+
+rem ***********************************************************************
+rem * 6. The TMP_FILE_DIRECTORY parameter defines the location where 
+rem *    temporary files (parsed raw data) will be placed. Default is  
+rem *    %TEMP%, which represents the system temporary directory.
+rem ***********************************************************************
+    set TMP_FILE_DIRECTORY=%TEMP%
+
+rem ***********************************************************************
+
+
+
+rem ***********************************************************************
+rem * It is not necessary to modify the following section.
+rem ***********************************************************************
+
 set JAVA_PARAMETERS=-XX:+UseParallelGC -Djava.io.tmpdir=%TMP_FILE_DIRECTORY% -Xms%HEAP_SIZE%m -Xmx%HEAP_SIZE%m -Djava.library.path="%JRI_LIB_PATH%"
-set CLASS_PATH=lib\${project.artifactId}-${project.version}.jar
+set CLASS_PATH=MZminePI.jar
 set MAIN_CLASS=net.sf.mzmine.main.MZmineCore
 
 rem Show java version, in case a problem occurs
-%JAVA_COMMAND% -version
+%JAVA_COMMAND% -version > MZminePI.log 2>&1
+echo. >> MZminePI.log
 
 rem This command starts the Java Virtual Machine
-echo %JAVA_COMMAND% %JAVA_PARAMETERS% -classpath %CLASS_PATH% %MAIN_CLASS% %*
-%JAVA_COMMAND% %JAVA_PARAMETERS% -classpath %CLASS_PATH% %MAIN_CLASS% %* > MZminePI.log 2>&1
+echo Running MZminePI...
+echo %JAVA_COMMAND% %JAVA_PARAMETERS% -classpath %CLASS_PATH% %MAIN_CLASS% %* >> MZminePI.log
+echo. >> MZminePI.log
+%JAVA_COMMAND% %JAVA_PARAMETERS% -classpath %CLASS_PATH% %MAIN_CLASS% %* >> MZminePI.log 2>&1
 
 rem If there was an error, give the user chance to see it
 IF ERRORLEVEL 1 (
 	echo See MZminePI.log for errors
 	pause
 )
+
+rem ***********************************************************************
