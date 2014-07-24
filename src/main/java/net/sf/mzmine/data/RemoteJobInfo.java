77cfb0e6a11ad3c818578cef4d4e429e55876deb
dan.schmidt <dan.schmidt@8f191f92-7a15-11e2-b7ab-c922b3a7170b>
2014-07-24 20:21:15 +0000

    Comments
    git-svn-id: https://tdi.codebasehq.com/protid/mzminepi.svn@251 8f191f92-7a15-11e2-b7ab-c922b3a7170b

--- src/main/java/net/sf/mzmine/data/RemoteJobInfo.java
+++ src/main/java/net/sf/mzmine/data/RemoteJobInfo.java
@@ -1,7 +1,7 @@
 /*
- * Copyright 2013-2013 The Veritomyx
+ * Copyright 2013-2014 Veritomyx
  * 
- * This file is part of MZmine 2.
+ * This file is part of MZminePI.
  * 
  * MZmine 2 is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
