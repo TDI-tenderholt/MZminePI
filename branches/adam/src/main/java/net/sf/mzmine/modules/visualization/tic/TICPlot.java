415a8d384a91910e60b57f6186782296a0ba0426
adam_tenderholt@targetdiscovery.com <adam_tenderholt@targetdiscovery.com@8f191f92-7a15-11e2-b7ab-c922b3a7170b>
2014-08-19 18:00:27 +0000

    Minor bug fix to TICPlot
    git-svn-id: https://tdi.codebasehq.com/protid/mzminepi.svn@333 8f191f92-7a15-11e2-b7ab-c922b3a7170b

--- branches/adam/src/main/java/net/sf/mzmine/modules/visualization/tic/TICPlot.java
+++ branches/adam/src/main/java/net/sf/mzmine/modules/visualization/tic/TICPlot.java
@@ -25,6 +25,7 @@ import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.Shape;
 import java.awt.Stroke;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
